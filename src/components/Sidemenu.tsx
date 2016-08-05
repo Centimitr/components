25eb78a74f53dfdb68d396d5389f95bf042b93b3
Xiao Shi <shx@cn.tradeshift.com>
2016-08-05 10:16:40 +0800

    add sidemenu, has issues 

--- src/components/Sidemenu.tsx
+++ src/components/Sidemenu.tsx
@@ -1,18 +1,30 @@
 import * as React from "react";
 
-export interface SidemenuProps { }
-export interface SidemenuState { }
+export interface SidemenuProps { defautFlatStatus?: boolean }
+export interface SidemenuState { flat: boolean }
 
 export class Sidemenu extends React.Component<SidemenuProps, SidemenuState> {
 
 	constructor(props: SidemenuProps) {
 		super(props);
-		this.state = {};
+		this.state = { flat: !!props.defautFlatStatus };
+	}
+
+	contract = () => {
+		this.setState({ flat: false });
+	}
+
+	flat = () => {
+		this.setState({ flat: true });
 	}
 
     render() {
+		let className = 'UISidemenu';
+		if (this.state.flat) {
+			className += ' ' + 'flat';
+		}
         return (
-			<div className="UISidemenu">
+			<div className={className} onMouseEnter={this.flat} onMouseOut={this.contract}>
 				{this.props.children}
 			</div>
         );
